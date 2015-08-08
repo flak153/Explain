a03f6aa604f1f97314f3fa003789bc15c0dbfc03
flak153 <flakgun153@gmail.com>
2015-08-08 16:43:20 -0400

    flask

--- myass.py
+++ myass.py
@@ -1,15 +1,27 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+if __name__ == '__main__':
+    app.run()
+
 from pymongo import MongoClient
 
 client = MongoClient()
 db = client.test
 
 from datetime import datetime
-result = db.restaurants.delete_many({})
-# result = db.restaurants.insert_one(
-#     {
-#         "restaurant_id": "41704620"
-#     }
-# )
-# cursor = db.restaurants.find()
-# for document in cursor:
-#     print(document)
+result = db.questions.delete_many({})
+result = db.questions.insert_one(
+    {
+    "OP": "41704620",
+    "Question": "41704620",
+
+    }
+)
+cursor = db.restaurants.find()
+for document in cursor:
+    print(document)
